747b67424b271c31cdbf9f9676a54433eae5612d
Renan Kanu <renankanu@gmail.com>
2021-05-02 17:14:14 -0300

    move api details from domain class to data class

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,3 +1,5 @@
+import 'package:clean_architecture_tdd_flutter/domain/entities/account_entity.dart';
+
 import '../../domain/usecases/usecases.dart';
 
 import '../http/http_client.dart';
@@ -9,6 +11,23 @@ class RemoteAuthentication {
   RemoteAuthentication({required this.httpClient, required this.url});
 
   Future<void> auth(AuthenticationParams params) async {
-    await httpClient.request(url: url, method: 'post', body: params.toJson());
+    final body = RemoteAuthenticationParams.fromDomain(params).toJson();
+    await httpClient.request(url: url, method: 'post', body: body);
   }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    required this.email,
+    required this.password,
+  });
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(
+          email: params.email, password: params.password);
+
+  Map toJson() => {'email': email, 'password': password};
 }
