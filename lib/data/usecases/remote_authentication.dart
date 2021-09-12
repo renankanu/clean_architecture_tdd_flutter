dee0e1d1de202a5181da017ae040f7b9f2b5b7fd
Renan Kanu <renankanu@gmail.com>
2021-09-12 12:59:57 -0300

    move api details from domain entity to data model

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -13,6 +13,30 @@ class RemoteAuthentication {
   });
 
   Future<void> auth(AuthenticationParams params) async {
-    await httpClient.request(url: url, method: method, body: params.toJson());
+    final body = RemoteAuthenticationParams.fromDomain(params).toJson();
+    await httpClient.request(
+      url: url,
+      method: method,
+      body: body,
+    );
   }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    required this.email,
+    required this.password,
+  });
+
+  factory RemoteAuthenticationParams.fromDomain(
+          AuthenticationParams authenticationParams) =>
+      RemoteAuthenticationParams(
+        email: authenticationParams.email,
+        password: authenticationParams.secret,
+      );
+
+  Map toJson() => {'email': email, 'password': password};
 }
