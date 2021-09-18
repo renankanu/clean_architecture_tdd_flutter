d39a174575cfed0389cb67a7db7a869dfbf58def
renankanu <renankanu@gmail.com>
2021-09-18 11:09:03 -0300

    ensure HttpAdapter return data if post retuns 204

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -103,5 +103,13 @@ void main() {
 
       expect(response, null);
     });
+
+    test('Should return null if post returns 204', () async {
+      mockResponseAny(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
   });
 }
